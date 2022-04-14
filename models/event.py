8ae962f57fe1bad635a952a3f1f1ac8acc89454a
Fiona <fiona_berkery@hotmail.com>
2022-04-14 13:43:48 +0100

    first half of extension

--- models/event.py
+++ models/event.py
@@ -1,7 +1,8 @@
 class Event():
-    def __init__(self, date, name, no_of_guests, room_location, description):
+    def __init__(self, date, name, no_of_guests, room_location, description, recurring):
         self.date = date
         self.name = name
         self.no__of_guests = no_of_guests
         self.room_location = room_location
         self.description = description
+        self.recurring = recurring
--- models/eventslist.py
+++ models/eventslist.py
@@ -1,8 +1,8 @@
 from models.event import *
 
-event1 = Event("14/04/22", "Alumni Event", 50, 1, "Codeclan Alumni meeting with current students & alumni")
-event2 = Event("30/05/22", "Sophie's Birthday", 200, 5, "Sophie's 30th birthday party with lots of friends" )
-event3 = Event("14/07/22", "Summer Drinks Party", 150, 10, "Summer themed work drinks party")
+event1 = Event("14/03/22", "Alumni Event", 50, 1, "Codeclan Alumni meeting with current students & alumni", True)
+event2 = Event("30/08/22", "Sophie's Birthday", 200, 5, "Sophie's 30th birthday party with lots of friends", False)
+event3 = Event("14/01/22", "Summer Drinks Party", 150, 10, "Summer themed work drinks party", True)
 
 events = [event1, event2, event3]
 
